b25da3a784d9a093b995bae8136a49d24dfe7b3e
RyUk346 <mahmudur346@gmail.com>
2025-07-30 19:42:30 +0600

    restaurant

---
+++ Restaurants/src/App.jsx
@@ -0,0 +1,28 @@
+import "./App.css";
+import Header from "./components/header";
+import Head from "./components/Head";
+import Home from "./components/Home";
+import About from "./components/About";
+import Foods from "./components/Foods";
+import Book from "./components/Book";
+import Feedback from "./components/Feedback";
+import Footer from "./components/Footer";
+import "flowbite";
+
+function App() {
+  return (
+    <>
+      <div className="sticky top-0 z-50">
+        <Head />
+      </div>
+      <Home></Home>
+      <About></About>
+      <Foods></Foods>
+      <Book></Book>
+      <Feedback></Feedback>
+      <Footer></Footer>
+    </>
+  );
+}
+
+export default App;
